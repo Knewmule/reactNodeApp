93a68b5fa62148bb90f69dfcac0365b32a86c16c
vivit <jack.problems@protonmail.com>
2024-08-26 23:11:20 -0700

    google returns a secret containing our profile and email information for google authentication login

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # reactNodeApp
+console.cloud.google.com
+Google O auth Client secret and Id encrypted
+-----BEGIN PGP MESSAGE-----
+
+jA0ECQMCFVxrkqW31TX/0sBEARVrN5XrPDSiYrX4C0J51AjqJ6tLcqNXeYKYwlLZ
+TiMYY9FYtRavW6YYieVrx41zi0aZ/Q3oDLAanFBMTq4Ry50sKbzOd6fp58dPIPh6
+QN1FAYBYomD0o1m/xA1VjdBOTQaru5s9sRWxQMnQt+P1sQ3J3EULKZjbnOJU4lik
+UhXvBo3Mg7PQqtG5z6VVqe67CQpuOT++NlLTe3spYhWtHilf/EqJRxcKr3wBxNCc
+JCqkUZCZaOqLKlrsEWIsrAwknkKGd8uc1CvcvNKldUHnJCQBTcqNkB5X3zNRL0wp
+r9mSJr3bTUGCGX/G//xolXrkmB0R1y1uSKX+UHFUU79u0geCu/Q=
+=GnhW
+-----END PGP MESSAGE-----
--- functions/app.js
+++ functions/app.js
@@ -1,24 +1,38 @@
 //app.js
-// Extra code added with serverless-http and Router() to deploy and build to netlify
-const express = require("express");
-const serverless = require("serverless-http");
-const app = express();
-const router = express.Router();
-
-router.get("/", (req, res) => {
-    res.send({hi:"hi there"});
-});
-
-app.use("/.netlify/functions/app", router);
-module.exports.handler = serverless(app);
-// Original code without the netlify upgrade
-// const express = require('express');
-// // import express from 'express';
+// Production code added with serverless-http and Router() to deploy and build to netlify
+// const express = require("express");
+// const serverless = require("serverless-http");
 // const app = express();
+// const router = express.Router();
 
-// app.get('/',(req,res)=>{
-//     res.send({hi:'there'});    
-// })
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT);
-// app.use()
+// router.get("/", (req, res) => {
+//     res.send({hi:"hi there"});
+// });
+
+// app.use("/.netlify/functions/app", router);
+// module.exports.handler = serverless(app);
+// Original dev code without the netlify upgrade
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys')
+const app = express();
+
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'    
+}, (accessToken) =>{
+    console.log(accessToken)
+    } 
+  )
+);
+app.get('/auth/google',passport.authenticate('google',{
+    scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
