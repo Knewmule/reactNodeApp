db4ed2d5592075bb3ca4324af60aeffa67de9169
vivit <jack.problems@protonmail.com>
2024-08-26 23:19:51 -0700

    now returns profile info

--- functions/app.js
+++ functions/app.js
@@ -22,8 +22,10 @@ passport.use(new GoogleStrategy({
     clientID:keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'    
-}, (accessToken) =>{
-    console.log(accessToken)
+}, (accessToken, refreshToken, profile, done) =>{
+    console.log('access Token ',accessToken);
+    console.log('refresh Token ', refreshToken);
+    console.log('profile: ', profile);
     } 
   )
 );
