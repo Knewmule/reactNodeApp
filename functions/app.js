4b4ba5d92cdb4cc1a1749d448149c9b8a8c714f9
vivit <jack.problems@protonmail.com>
2024-08-28 18:56:09 -0700

    netlify deploy to production branch after netlify preview

--- functions/app.js
+++ functions/app.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('../config/keys');
+//2 Netlify lines
+const serverless = require("serverless-http");
+const router = express.Router();
+
+
 
 require('../models/Users');
 require('../services/passport');
@@ -22,10 +27,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('../routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
-
+// app.listen(PORT);
+//Netlify Code
+app.use("/.netlify/functions/app", router);
+module.exports.handler = serverless(app);
 
 
 
